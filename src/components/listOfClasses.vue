4d5347e24bf684f47dd8c80606e9feceb1ac7f49
Vadkarika2 <40302952+Vadkarika2@users.noreply.github.com>
2021-03-10 15:37:09 +0100

    ui styling upgrades and analytics

--- src/components/listOfClasses.vue
+++ src/components/listOfClasses.vue
@@ -4,6 +4,7 @@
             <v-row class="d-flex justify-center">
                 <v-col cols="10">
                     <v-text-field label="Óra neve" outlined dark v-model="newItem" @keyup.enter="addItem"></v-text-field>
+                    <v-text-field label="Óra ídőtartama" outlined dark v-model="newTimestamp" @keyup.enter="addItem"></v-text-field>
                     <v-text-field label="Link a belépéshez" outlined dark v-model="newLink" @keyup.enter="addItem"></v-text-field>
                 </v-col>
             </v-row>
@@ -15,11 +16,13 @@
             <h1>-</h1>
         </v-row>
         <transition-group name="fade">
-            <v-card class="mx-auto pa-3 ma-2 text-center" max-width="600" v-for="room in rooms" :key="room.id" color="#262626" dark>
+            <v-card class="mx-auto pa-3 ma-2 text-center" max-width="350" v-for="room in rooms" :key="room.id" color="#262626" dark>
                 <v-row align-content="center">
                     <v-col align-self="center">
                         <v-list-item-title class="headline mb-1">{{ room.name }}</v-list-item-title>
-                        <a :href="room.link">{{ room.link }}</a>
+                        <v-list-item-subtitle class="sub">{{ room.time }}</v-list-item-subtitle>
+                        <div class="invis">-</div>
+                        <v-btn color="green" @click="analyticsLog(room.name, room.link)">Belépés</v-btn>
                     </v-col>
                 </v-row>
             </v-card>
@@ -35,15 +38,21 @@ export default {
             rooms: [],
             newItem: "",
             newLink: "",
+            newTimestamp: "",
         }
     },
     methods: {
         async addItem() {
             if(this.newItem) {
-                await db.collection("classes").add({ name: this.newItem, link: this.newLink });
+                await db.collection("classes").add({ name: this.newItem, link: this.newLink, time: this.newTimestamp });
                 this.newItem = "";
                 this.newLink = "";
+                this.newTimestamp = "";
             }
+        },
+        async analyticsLog(name, link) {
+            open(link);
+            await db.collection("analytics").add({ name: name, link: link });
         },
         deleteRoom(id) {
             db.collection("classes").doc(id).delete();
@@ -58,3 +67,15 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+.sub {
+    color: rgb(105, 105, 105);
+};
+
+.invis {
+    color: rgba(0, 0, 0, 0);
+    height: 10px;
+    padding: center;
+};
+</style>
